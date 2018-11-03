5508063d3cd98fa2b0bbadd55bad33920be25460
rileylyman <rileylyman@berkeley.edu>
2018-11-03 06:07:38 -0700

    Added FieldElement trait

--- fft/src/lib.rs
+++ fft/src/lib.rs
@@ -1,3 +1,11 @@
+
+pub trait FieldElement<Element = Self> {
+	fn add(&self, other: &Element) -> Element;
+	fn sub(&self, other: &Element) -> Element;
+	fn mul(&self, other: &Element) -> Element;
+	fn div(&self, other: &Element) -> Element;
+}
+
 // field modulo 2147473697
 #[derive(Debug)]
 struct Gf32 {
@@ -8,28 +16,28 @@ const MODULUS_GF32 : u32 = 2147473697;
 //const MODULUS_GF32 : u32 = 15;
 
 
-impl Gf32 {
-    fn add(&self, a: Gf32) -> Gf32 {
-        Gf32{
-                value: (self.value + a.value) % MODULUS_GF32
+impl FieldElement<Gf32> for Gf32 {
+    fn add(&self, other: &Gf32) -> Gf32 {
+		Gf32{
+                value: (self.value + other.value) % MODULUS_GF32
             }
     }
 
-    fn sub(&self, a: Gf32) -> Gf32 {
+    fn sub(&self, other: &Gf32) -> Gf32 {
         Gf32{
-                value: (self.value - a.value) % MODULUS_GF32
+                value: (self.value - other.value) % MODULUS_GF32
             }
     }
 
-    fn mul(&self, a: Gf32) -> Gf32 {
+    fn mul(&self, other: &Gf32) -> Gf32 {
         Gf32{
-                value: (((self.value as u64) * (a.value as u64)) % (MODULUS_GF32 as u64)) as u32
+                value: (((self.value as u64) * (other.value as u64)) % (MODULUS_GF32 as u64)) as u32
             }
     }
 
-    fn div(&self, a: Gf32) -> Gf32 {
+    fn div(&self, other: &Gf32) -> Gf32 {
         let x = MODULUS_GF32 as i32;
-        let y = a.value as i32;
+        let y = other.value as i32;
         
         let (gcd, x_coeff, y_coeff) = egcd(x, y);
 
